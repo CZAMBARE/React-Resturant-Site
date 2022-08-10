61b7fcdd164ffaf8001bfe4e091f27c2547a862f
Chetan Zambare <zambare.chetan123@gmil.com>
2022-08-10 09:25:01 +0530

    Component part 2, Prop efine, Elevate the state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent'
 import './App.css';
+import { DISHES } from './shared/dishes';
+import { Component } from 'react';
 
-function App() {
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+
+  render(){
   return (
     <div className="App">
       <Navbar dark color="primary">
@@ -11,8 +25,10 @@ function App() {
           <NavbarBrand href="/">Ristontrate con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu  dishes={this.state.dishes} />
     </div>
   );
+  }
 }
 
 export default App;
